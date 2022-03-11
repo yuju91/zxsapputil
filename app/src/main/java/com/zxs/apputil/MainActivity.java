7a64fd822c0de7152d42eb7bba6674278450d4f6
zhangxvsheng <707058607@qq.com>
2022-03-11 14:41:27 +0800

    初始化

--- app/src/main/java/com/zxs/apputil/MainActivity.java
+++ app/src/main/java/com/zxs/apputil/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.zxs.mylibrary.Valid;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Valid.isPhone("122");
     }
 }
